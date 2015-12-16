507aaf3d8aeeaa621fc155499cdff553479ed14b
Dave Eddy <dave@daveeddy.com>
2015-12-16 13:47:01 -0500

    license headers

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright 2015, Joyent, Inc.
+ */
+
 var execFile = require('child_process').execFile;
 var zonename = require('../lib');
 
